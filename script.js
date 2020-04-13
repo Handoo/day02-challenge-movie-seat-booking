f0c657e688c0b1367b7ed474061dc0a094cc0347
Hans <r.hans4486@gmail.com>
2020-04-12 23:10:56 -0400

    Setup localStorage data

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
+
+// Save selected movie and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem( 'selectedMovieIndex', movieIndex );
+    localStorage.setItem( 'selectedMoviePrice', moviePrice );
+}
 
 // Function update total and count
 function updateCount() {
@@ -11,20 +17,29 @@ function updateCount() {
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-    return selectedSeatsCount;  
+
+    // Make things ready for local storage
+    const seatsIndex = [...selectedSeats].map(function(seat) {
+        return [...seats].indexOf(seat);
+    });
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 }
 
+// Event when a user change the ticket price
+movieSelect.addEventListener('change', function(event) {
+    ticketPrice = +event.target.value;
+    setMovieData(event.target.selectedIndex, event.target.value);
+    updateCount();
+});
+
+// Event when user click on a seat
 container.addEventListener('click', function(event) {
     if (event.target.classList.contains('seat') && !event.target.classList.contains('occupied')) {
         event.target.classList.toggle('selected');
-
         updateCount();
     }
     
 });
 
-movieSelect.addEventListener('change', function(event) {
-    ticketPrice = +event.target.value;
 
-    updateCount();
-});
